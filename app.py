2a6f0b95baea554fd7bd0f2c2ef7f97bb406b414
AI-ANK <130707158+AI-ANK@users.noreply.github.com>
2023-12-16 02:37:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -86,7 +86,9 @@ class StreamlitChatPack(BaseLlamaPack):
             sql_database = SQLDatabase(engine) #include all tables
 
             # Initialize LLM
-            llm2 = PaLM(api_key=os.environ["GOOGLE_API_KEY"])  # Replace with your API key
+            #llm2 = PaLM(api_key=os.environ["GOOGLE_API_KEY"])  # Replace with your API key
+            llm2 = OpenAI(temperature=0.1, model="gpt-3.5-turbo-1106")
+
             service_context = ServiceContext.from_defaults(llm=llm2, embed_model="local")
             
             return sql_database, service_context, engine
